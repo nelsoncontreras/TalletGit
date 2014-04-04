bf0131087ce98c5742748271c7a472af8ec1c6ed
Alumno <Alumno@EQ03-LAB1>
2014-04-04 11:18:55 -0300

    Segundo commit

--- TallerGit/src/cl/taller/git/Main.java
+++ TallerGit/src/cl/taller/git/Main.java
@@ -3,8 +3,6 @@ package cl.taller.git;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-    System.out.println("Primer Commit");
+    System.out.println("Segundo Commit");
 	}
-
 }
